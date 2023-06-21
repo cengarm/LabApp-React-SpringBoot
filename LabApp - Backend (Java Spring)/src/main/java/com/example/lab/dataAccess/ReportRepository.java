e1ab5974c71bad1ad9b51ed33d83c7e9f942a0e6
Mustafa Cengar <126611512+cengarm@users.noreply.github.com>
2023-06-21 13:12:59 +0300

    Delete LabApp - Backend (Java Spring) directory

--- LabApp - Backend (Java Spring)/src/main/java/com/example/lab/dataAccess/ReportRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.lab.dataAccess;
-
-import com.example.lab.entity.Report;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReportRepository extends JpaRepository<Report,Long> {
-
-}
